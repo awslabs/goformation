d2a8e13825c468252c576c36b1174c335ca2c959
AWS GoFormation <goformation@amazon.com>
2017-09-08 14:06:22 +0100

    Updated create-pull-request script to work with a dedicated GitHub goformation user rather than my personal one

--- generate/create-pull-request.sh
+++ generate/create-pull-request.sh
@@ -2,15 +2,26 @@
 
 # This script runs on a regular cron within Travis-CI.
 # 
-# It checks to see if any AWS CloudFormation resources have been updated, 
-# and if so, creates a new pull request to apply them back to the awslabs repo.
+# It does the following:
+# 
+# 1. Clone github.com/goformation/goformation to local filesystem
+# 2. Switch to aws-goformation-updates branch (creating it if necessary)
+# 3. Merge in any changes from github.com/awslabs/goformation (upstream)
+# 4. Run 'go generate'
+# 5. Check whether any resources changed
+# 6. Run 'go test' to ensure all tests pass
+# 7. Commit any changes and push to github.com/goformation/goformation
+# 8. Create a pull request from github.com/goformation/goformation (branch: aws-goformation-updates) to github.com/awslabs/goformation (master)
+#
+# It relies on the environment GITHUB_TOKEN containing a Personal Access Token for the github.com/goformation user.
 
 # Bomb out on errors
-set -e
+#set -e
 
 # The repo/branch to create the PR against
-REPO="awslabs/goformation"
+SRC_REPO="goformation/goformation"
 SRC_BRANCH="master"
+DST_REPO="awslabs/goformation"
 DST_BRANCH="master"
 
 # Git details (for the commit)
@@ -34,10 +45,10 @@ fi
 git config --global user.name "${COMMIT_NAME}"
 git config --global user.email "${COMMIT_EMAIL}"
 
-echo "Checking out github.com/${REPO}..."
+echo "Checking out github.com/${SRC_REPO}..."
 UPSTREAM_DIR=/tmp/upstream/src/github.com/awslabs/goformation
 mkdir -p ${UPSTREAM_DIR}
-git clone https://github.com/${REPO}.git ${UPSTREAM_DIR} > /dev/null 2>&1
+git clone https://github.com/${SRC_REPO}.git ${UPSTREAM_DIR} > /dev/null 2>&1
 GOPATH=/tmp/upstream
 cd ${UPSTREAM_DIR}
 
@@ -46,6 +57,11 @@ REQUEST_BRANCH="aws-goformation-updates"
 echo "Creating new branch: ${REQUEST_BRANCH} tracking origin/${SRC_BRANCH}"
 git checkout -b ${REQUEST_BRANCH} origin/${SRC_BRANCH}
 git pull --rebase origin ${REQUEST_BRANCH} || true
+
+# Merging in any changes from upstream
+git remote add upstream https://github.com/${DST_REPO}.git
+git pull --rebase upstream ${SRC_BRANCH} || true
+
  
 echo "Auto-generating AWS CloudFormation resources..."
 go generate
@@ -67,7 +83,7 @@ git add cloudformation/*
 git commit -m "${COMMIT_MSG}" 
 
 echo "Pushing changes..."
-git remote add origin-push https://${GITHUB_TOKEN}@github.com/${REPO}.git > /dev/null 2>&1
+git remote add origin-push https://${GITHUB_TOKEN}@github.com/${SRC_REPO}.git > /dev/null 2>&1
 git push --quiet --set-upstream origin-push ${REQUEST_BRANCH}
 
 echo "Installing GitHub Hub"
