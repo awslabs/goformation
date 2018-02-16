7dc5ba2f4bc96fa49726169cb663cd2497cdba21
parsnips <parsnips@gmail.com>
2018-02-15 20:57:25 -0800

    Transforms can be lists or single string.

--- cloudformation/template.go
+++ cloudformation/template.go
@@ -10,7 +10,7 @@ import (
 // see: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/template-anatomy.html
 type Template struct {
 	AWSTemplateFormatVersion string                 `json:"AWSTemplateFormatVersion,omitempty"`
-	Transform                string                 `json:"Transform,omitempty"`
+	Transform                Transform              `json:"Transform,omitempty"`
 	Description              string                 `json:"Description,omitempty"`
 	Metadata                 map[string]interface{} `json:"Metadata,omitempty"`
 	Parameters               map[string]interface{} `json:"Parameters,omitempty"`
@@ -18,6 +18,46 @@ type Template struct {
 	Conditions               map[string]interface{} `json:"Conditions,omitempty"`
 	Resources                map[string]interface{} `json:"Resources,omitempty"`
 	Outputs                  map[string]interface{} `json:"Outputs,omitempty"`
+}
+
+type Transform struct {
+	String *string
+
+	StringArray *[]string
+}
+
+func (t Transform) value() interface{} {
+	if t.String != nil {
+		return t.String
+	}
+
+	if t.StringArray != nil {
+		return t.StringArray
+	}
+
+	return nil
+}
+
+func (t *Transform) MarshalJSON() ([]byte, error) {
+	return json.Marshal(t.value())
+}
+
+func (t *Transform) UnmarshalJSON(b []byte) error {
+	var typecheck interface{}
+	if err := json.Unmarshal(b, &typecheck); err != nil {
+		return err
+	}
+
+	switch val := typecheck.(type) {
+
+	case string:
+		t.String = &val
+
+	case []string:
+		t.StringArray = &val
+	}
+
+	return nil
 }
 
 // NewTemplate creates a new AWS CloudFormation template struct
