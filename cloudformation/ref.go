d4f52ba45c9f61fdba267a955c9222500d673858
Paul Maddox <pmaddox@amazon.com>
2018-09-13 06:36:48 +0100

    Don't rely on magic strings, instead have intrinsics as strings, and unmarshal to objects in post-processing. This also has the benefit of easily supporting all intrinsic functions

--- cloudformation/ref.go
+++ cloudformation/ref.go
@@ -2,17 +2,21 @@ package cloudformation
 
 import (
 	"encoding/json"
-	"regexp"
 )
 
 // Ref creates a CloudFormation Reference to another resource in the template
 func Ref(logicalName string) string {
-	return "%%Ref:" + logicalName + "%%"
+	return `{ "Ref": "` + logicalName + `" }`
 }
 
-// GetAtt creates a CloudFormation reference to an attribute of another resource in the template
+// GetAtt returns the value of an attribute from a resource in the template.
 func GetAtt(logicalName string, attribute string) string {
-	return "%%Fn::GetAtt:" + logicalName + "." + attribute + "%%"
+	return `{ "Fn::GetAtt": [ "` + logicalName + `", "` + attribute + `" ] }`
+}
+
+// ImportValue returns the value of an output exported by another stack. You typically use this function to create cross-stack references. In the following example template snippets, Stack A exports VPC security group values and Stack B imports them.
+func ImportValue(name string) string {
+	return `{ "Fn::Import": "` + name + `" }`
 }
 
 // processReferences is a post processor that replaces all goformation references
@@ -56,28 +60,52 @@ func replaceReferencesRecursive(input interface{}) interface{} {
 
 	case string:
 
-		// Check if the string contains a goformation reference
-		refexp := regexp.MustCompile("%%Ref:(.*)%%")
-		refmatches := refexp.FindStringSubmatch(value)
-		if len(refmatches) > 0 {
-			return map[string]string{
-				"Ref": refmatches[1],
+		// Check if the string can be unmarshalled into an intrinsic object
+		var intrinsic map[string]interface{}
+		if err := json.Unmarshal([]byte(value), &intrinsic); err != nil {
+			// The string value is not JSON, so it's not an intrinsic so just pass it back
+			return value
+		}
+
+		// An intrinsic should be an object, with a single key containing a valid intrinsic name
+		if len(intrinsic) != 1 {
+			return value
+		}
+
+		supported := []string{
+			"Ref",
+			"Fn::Base64",
+			"Fn::Cidr",
+			"Fn::FindInMap",
+			"Fn::GetAtt",
+			"Fn::GetAZs",
+			"Fn::ImportValue",
+			"Fn::Join",
+			"Fn::Select",
+			"Fn::Split",
+			"Fn::Sub",
+			"Fn::Transform",
+			"Fn::And",
+			"Fn::Equals",
+			"Fn::If",
+			"Fn::Not",
+			"Fn::Or",
+		}
+
+		for name, v := range intrinsic {
+			for _, i := range supported {
+				if name == i {
+					return map[string]interface{}{
+						name: replaceReferencesRecursive(v),
+					}
+				}
 			}
 		}
 
-		// Check if the string containers a goformation GetAtt reference
-		getattexp := regexp.MustCompile(`%%Fn::GetAtt:(.*)\.(.*)%%`)
-		getattmatches := getattexp.FindStringSubmatch(value)
-		if len(getattmatches) > 0 {
-			return map[string][]string{
-				"Fn::GetAtt": []string{getattmatches[1], getattmatches[2]},
-			}
-		}
 		return value
 
 	default:
 		return value
-
 	}
 
 }
