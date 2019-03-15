17c706e037d50128c7c3c07a24c331d69db12dd0
AWS GoFormation <goformation@amazon.com>
2019-03-15 04:06:25 +0000

    feat(schema): AWS CloudFormation Update (2019-03-15)

---
+++ cloudformation/resources/aws-greengrass-functiondefinition_functiondefinitionversion.go
@@ -0,0 +1,62 @@
+package resources
+
+import "github.com/awslabs/goformation/cloudformation/policies"
+
+// AWSGreengrassFunctionDefinition_FunctionDefinitionVersion AWS CloudFormation Resource (AWS::Greengrass::FunctionDefinition.FunctionDefinitionVersion)
+// See: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-greengrass-functiondefinition-functiondefinitionversion.html
+type AWSGreengrassFunctionDefinition_FunctionDefinitionVersion struct {
+
+	// DefaultConfig AWS CloudFormation Property
+	// Required: false
+	// See: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-greengrass-functiondefinition-functiondefinitionversion.html#cfn-greengrass-functiondefinition-functiondefinitionversion-defaultconfig
+	DefaultConfig *AWSGreengrassFunctionDefinition_DefaultConfig `json:"DefaultConfig,omitempty"`
+
+	// Functions AWS CloudFormation Property
+	// Required: true
+	// See: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-greengrass-functiondefinition-functiondefinitionversion.html#cfn-greengrass-functiondefinition-functiondefinitionversion-functions
+	Functions []AWSGreengrassFunctionDefinition_Function `json:"Functions,omitempty"`
+
+	// _deletionPolicy represents a CloudFormation DeletionPolicy
+	_deletionPolicy policies.DeletionPolicy
+
+	// _dependsOn stores the logical ID of the resources to be created before this resource
+	_dependsOn []string
+
+	// _metadata stores structured data associated with this resource
+	_metadata map[string]interface{}
+}
+
+// AWSCloudFormationType returns the AWS CloudFormation resource type
+func (r *AWSGreengrassFunctionDefinition_FunctionDefinitionVersion) AWSCloudFormationType() string {
+	return "AWS::Greengrass::FunctionDefinition.FunctionDefinitionVersion"
+}
+
+// DependsOn returns a slice of logical ID names this resource depends on.
+// see: https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-attribute-dependson.html
+func (r *AWSGreengrassFunctionDefinition_FunctionDefinitionVersion) DependsOn() []string {
+	return r._dependsOn
+}
+
+// SetDependsOn specify that the creation of this resource follows another.
+// see: https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-attribute-dependson.html
+func (r *AWSGreengrassFunctionDefinition_FunctionDefinitionVersion) SetDependsOn(dependencies []string) {
+	r._dependsOn = dependencies
+}
+
+// Metadata returns the metadata associated with this resource.
+// see: https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-attribute-metadata.html
+func (r *AWSGreengrassFunctionDefinition_FunctionDefinitionVersion) Metadata() map[string]interface{} {
+	return r._metadata
+}
+
+// SetMetadata enables you to associate structured data with this resource.
+// see: https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-attribute-metadata.html
+func (r *AWSGreengrassFunctionDefinition_FunctionDefinitionVersion) SetMetadata(metadata map[string]interface{}) {
+	r._metadata = metadata
+}
+
+// SetDeletionPolicy applies an AWS CloudFormation DeletionPolicy to this resource
+// see: https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-attribute-deletionpolicy.html
+func (r *AWSGreengrassFunctionDefinition_FunctionDefinitionVersion) SetDeletionPolicy(policy policies.DeletionPolicy) {
+	r._deletionPolicy = policy
+}
---
+++ cloudformation/resources/aws-greengrass-subscriptiondefinitionversion_subscription.go
@@ -0,0 +1,72 @@
+package resources
+
+import "github.com/awslabs/goformation/cloudformation/policies"
+
+// AWSGreengrassSubscriptionDefinitionVersion_Subscription AWS CloudFormation Resource (AWS::Greengrass::SubscriptionDefinitionVersion.Subscription)
+// See: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-greengrass-subscriptiondefinitionversion-subscription.html
+type AWSGreengrassSubscriptionDefinitionVersion_Subscription struct {
+
+	// Id AWS CloudFormation Property
+	// Required: true
+	// See: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-greengrass-subscriptiondefinitionversion-subscription.html#cfn-greengrass-subscriptiondefinitionversion-subscription-id
+	Id string `json:"Id,omitempty"`
+
+	// Source AWS CloudFormation Property
+	// Required: true
+	// See: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-greengrass-subscriptiondefinitionversion-subscription.html#cfn-greengrass-subscriptiondefinitionversion-subscription-source
+	Source string `json:"Source,omitempty"`
+
+	// Subject AWS CloudFormation Property
+	// Required: true
+	// See: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-greengrass-subscriptiondefinitionversion-subscription.html#cfn-greengrass-subscriptiondefinitionversion-subscription-subject
+	Subject string `json:"Subject,omitempty"`
+
+	// Target AWS CloudFormation Property
+	// Required: true
+	// See: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-greengrass-subscriptiondefinitionversion-subscription.html#cfn-greengrass-subscriptiondefinitionversion-subscription-target
+	Target string `json:"Target,omitempty"`
+
+	// _deletionPolicy represents a CloudFormation DeletionPolicy
+	_deletionPolicy policies.DeletionPolicy
+
+	// _dependsOn stores the logical ID of the resources to be created before this resource
+	_dependsOn []string
+
+	// _metadata stores structured data associated with this resource
+	_metadata map[string]interface{}
+}
+
+// AWSCloudFormationType returns the AWS CloudFormation resource type
+func (r *AWSGreengrassSubscriptionDefinitionVersion_Subscription) AWSCloudFormationType() string {
+	return "AWS::Greengrass::SubscriptionDefinitionVersion.Subscription"
+}
+
+// DependsOn returns a slice of logical ID names this resource depends on.
+// see: https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-attribute-dependson.html
+func (r *AWSGreengrassSubscriptionDefinitionVersion_Subscription) DependsOn() []string {
+	return r._dependsOn
+}
+
+// SetDependsOn specify that the creation of this resource follows another.
+// see: https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-attribute-dependson.html
+func (r *AWSGreengrassSubscriptionDefinitionVersion_Subscription) SetDependsOn(dependencies []string) {
+	r._dependsOn = dependencies
+}
+
+// Metadata returns the metadata associated with this resource.
+// see: https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-attribute-metadata.html
+func (r *AWSGreengrassSubscriptionDefinitionVersion_Subscription) Metadata() map[string]interface{} {
+	return r._metadata
+}
+
+// SetMetadata enables you to associate structured data with this resource.
+// see: https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-attribute-metadata.html
+func (r *AWSGreengrassSubscriptionDefinitionVersion_Subscription) SetMetadata(metadata map[string]interface{}) {
+	r._metadata = metadata
+}
+
+// SetDeletionPolicy applies an AWS CloudFormation DeletionPolicy to this resource
+// see: https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-attribute-deletionpolicy.html
+func (r *AWSGreengrassSubscriptionDefinitionVersion_Subscription) SetDeletionPolicy(policy policies.DeletionPolicy) {
+	r._deletionPolicy = policy
+}
