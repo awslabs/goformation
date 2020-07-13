6c6589e2c897a889244ac54361b1e0eca82fa651
Cedric Cordenier <cedric.cordenier@coinbase.com>
2020-07-13 19:47:07 +0100

    feat(schema): Add new DynamoDBEvent options

--- cloudformation/serverless/aws-serverless-function_dynamodbevent.go
+++ cloudformation/serverless/aws-serverless-function_dynamodbevent.go
@@ -13,10 +13,40 @@ type Function_DynamoDBEvent struct {
 	// See: https://github.com/awslabs/serverless-application-model/blob/master/versions/2016-10-31.md#dynamodb
 	BatchSize int `json:"BatchSize,omitempty"`
 
+	// BisectBatchOnFunctionError AWS CloudFormation Property
+	// Required: false
+	// See: https://github.com/awslabs/serverless-application-model/blob/master/versions/2016-10-31.md#dynamodb
+	BisectBatchOnFunctionError bool `json:"BisectBatchOnFunctionError,omitempty"`
+
+	// DestinationConfig AWS CloudFormation Property
+	// Required: false
+	// See: https://github.com/awslabs/serverless-application-model/blob/master/versions/2016-10-31.md#dynamodb
+	DestinationConfig *Function_DestinationConfig `json:"DestinationConfig,omitempty"`
+
 	// Enabled AWS CloudFormation Property
 	// Required: false
 	// See: https://github.com/awslabs/serverless-application-model/blob/master/versions/2016-10-31.md#dynamodb
 	Enabled bool `json:"Enabled,omitempty"`
+
+	// MaximumBatchingWindowInSeconds AWS CloudFormation Property
+	// Required: false
+	// See: https://github.com/awslabs/serverless-application-model/blob/master/versions/2016-10-31.md#dynamodb
+	MaximumBatchingWindowInSeconds int `json:"MaximumBatchingWindowInSeconds,omitempty"`
+
+	// MaximumRecordAgeInSeconds AWS CloudFormation Property
+	// Required: false
+	// See: https://github.com/awslabs/serverless-application-model/blob/master/versions/2016-10-31.md#dynamodb
+	MaximumRecordAgeInSeconds int `json:"MaximumRecordAgeInSeconds,omitempty"`
+
+	// MaximumRetryAttempts AWS CloudFormation Property
+	// Required: false
+	// See: https://github.com/awslabs/serverless-application-model/blob/master/versions/2016-10-31.md#dynamodb
+	MaximumRetryAttempts int `json:"MaximumRetryAttempts,omitempty"`
+
+	// ParallelizationFactor AWS CloudFormation Property
+	// Required: false
+	// See: https://github.com/awslabs/serverless-application-model/blob/master/versions/2016-10-31.md#dynamodb
+	ParallelizationFactor int `json:"ParallelizationFactor,omitempty"`
 
 	// StartingPosition AWS CloudFormation Property
 	// Required: true
