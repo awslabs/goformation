354228a9fc9de3c8b9fe32365884fe430eaa11ab
stephen.raymond <stephen.raymond@workday.com>
2021-05-25 17:20:13 -0600

    resource policy

--- cloudformation/serverless/aws-serverless-function_auth.go
+++ cloudformation/serverless/aws-serverless-function_auth.go
@@ -10,18 +10,23 @@ type Function_Auth struct {
 
 	// ApiKeyRequired AWS CloudFormation Property
 	// Required: false
-	// See: https://github.com/aws/serverless-application-model/blob/master/versions/2016-10-31.md#function-auth-object
+	// See: https://github.com/awslabs/serverless-application-model/blob/master/versions/2016-10-31.md#function-auth-object
 	ApiKeyRequired bool `json:"ApiKeyRequired,omitempty"`
 
 	// AuthorizationScopes AWS CloudFormation Property
 	// Required: false
-	// See: https://github.com/aws/serverless-application-model/blob/master/versions/2016-10-31.md#function-auth-object
+	// See: https://github.com/awslabs/serverless-application-model/blob/master/versions/2016-10-31.md#function-auth-object
 	AuthorizationScopes []string `json:"AuthorizationScopes,omitempty"`
 
 	// Authorizer AWS CloudFormation Property
 	// Required: false
-	// See: https://github.com/aws/serverless-application-model/blob/master/versions/2016-10-31.md#function-auth-object
+	// See: https://github.com/awslabs/serverless-application-model/blob/master/versions/2016-10-31.md#function-auth-object
 	Authorizer string `json:"Authorizer,omitempty"`
+
+	// ResourcePolicy AWS CloudFormation Property
+	// Required: false
+	// See: https://github.com/awslabs/serverless-application-model/blob/master/versions/2016-10-31.md#function-auth-object
+	ResourcePolicy *Function_AuthResourcePolicy `json:"ResourcePolicy,omitempty"`
 
 	// AWSCloudFormationDeletionPolicy represents a CloudFormation DeletionPolicy
 	AWSCloudFormationDeletionPolicy policies.DeletionPolicy `json:"-"`
