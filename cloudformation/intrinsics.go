286dd4c339fe9c64e31eb14d8eb621fad205fa7a
Marcin Rabenda <xrn.design@gmail.com>
2022-07-11 19:36:25 +0200

    fix(intrinsics): split function

--- cloudformation/intrinsics.go
+++ cloudformation/intrinsics.go
@@ -193,7 +193,7 @@ func GetAttPtr(logicalName string, attribute string) *string {
 
 // Split splits a string into a list of string values so that you can select an element from the resulting string list, use the Fn::Split intrinsic function. Specify the location of splits with a delimiter, such as , (a comma). After you split a string, use the Fn::Select function to pick a specific element.
 func Split(delimiter, source interface{}) string {
-	return fmt.Sprintf(`{ "Fn::Split" : [ %q, %q ] }`, delimiter, source)
+	return encode(fmt.Sprintf(`{ "Fn::Split" : [ %q, %q ] }`, delimiter, source))
 }
 
 func SplitPtr(delimiter, source interface{}) *[]string {
